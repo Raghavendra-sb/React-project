6e2cc9040b3f6f72149092a1f7a918f17da87c52
Raghavendra-sb <raghavendra.sb.07@gmail.com>
2025-03-25 22:41:59 +0530

    configurations

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+function App() {
+  let [count, setCount]=useState(0);
+ 
+ 
+  const addValue = ()=> {
+      if(count<20)
+      {
+        setCount(count+1);
+      }
+      else
+      {
+        count = 20
+      }
+  }
+
+  const devValue = () => {
+    
+    if(count>0)
+    {
+      setCount(count-1)
+    }
+    else{
+      count = 0;
+    }
+    
+  }
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>counter value : {count} </h2>
+      <button onClick={addValue}>up</button>
+      <button onClick={devValue}>down</button>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import Chai from "./raghu.jsx"
+function App() {
+ 
+
+  return (
+      <>  <Chai/>
+   <h1>Hi raghu</h1></>
+  )
+}
+
+export default App
